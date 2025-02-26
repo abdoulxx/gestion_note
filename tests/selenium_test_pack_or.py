90120529f79277b2e24114a925aaf21c7e89d1a1
SAMB SIDY <100100348+abdoulxx@users.noreply.github.com>
2025-02-26 14:28:37 +0000

    Create selenium_test_pack_or.py

---
+++ tests/selenium_test_pack_or.py
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/abdoulxx/gestion_note.git'
+            }
+        }
+        stage('Install Dependencies') {
+            steps {
+                bat 'composer install'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo "Build stage: Compilation / Packaging (si nécessaire)"
+            }
+        }
+        stage('Run Selenium Tests - Test 1') {
+            steps {
+                echo "Exécution du premier test Selenium"
+                bat '"C:\\Users\\aboul\\AppData\\Local\\Programs\\Python\\Python313\\python.exe" tests\\selenium_tests.py'
+            }
+        }
+        stage('Run Selenium Tests - Test 2') {
+            steps {
+                echo "Exécution du deuxième test Selenium"
+                bat '"C:\\Users\\aboul\\AppData\\Local\\Programs\\Python\\Python313\\python.exe" tests\\selenium_test_pack_or.py'
+            }
+        }
+    }
+    post {
+        always {
+            echo "Pipeline terminé"
+        }
+    }
+}
